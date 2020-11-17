c32ac9e1e43f8cab2503607d3465e293c3701753
Cora-Jane Meade <corajanemeade@gmail.com>
2020-11-17 15:24:32 +0000

    refactor

--- DinnerTonight/ViewController.swift
+++ DinnerTonight/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     var dinner = ""
 
-//let ballArray = [#imageLiteral(resourceName: "Background22"),#imageLiteral(resourceName: "Background15"),#imageLiteral(resourceName: "Background20"),#imageLiteral(resourceName: "Background12"),#imageLiteral(resourceName: "Background18"),#imageLiteral(resourceName: "Background21"),#imageLiteral(resourceName: "Background15"),#imageLiteral(resourceName: "Background20"),]
-
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var tapButton: UIButton!
@@ -20,13 +18,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         updateUI()
         imageView.image = UIImage(named:"Background11")!
+        dinnerDetails.isEnabled = false //this will eventually connected to a Dinner object
     }
 
     @IBAction func askButtonPressed(_ sender: UIButton) {
         
         updateUI()
+        
         let dinners = ["Spag Bol", "Creamy Mushroom Pasta", "Stuffed Peppers", "Meatballs", "Portobello Mushroom Pesto Pasta", "Teriyaki Salmon", "Vegetable Tagine with Couscous", "Chilli Con Carne", "Chorizo Prawn Linguine"]
         dinner = dinners[Int.random(in: 0...(dinners.count - 1))]
+        
         let pictureNumber = Int.random(in: 2...22)
         imageView.image = UIImage(named:"Background\(pictureNumber)")!
     
@@ -34,13 +35,11 @@ class ViewController: UIViewController {
         
         //Disappear and reappear when the details appear?
         //titleLabel.text = ""
+        //dinnerDetails.isHidden = true
         //tapButton.isHidden = true
         
     }
     
-    @IBAction func dinnerDetailsPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "ShowDetail", sender: sender)
-    }
     
     func updateUI() {
         imageView.contentMode = .scaleAspectFill
@@ -56,20 +55,9 @@ class ViewController: UIViewController {
         // imageView.contentMode = .right //8
         
         //Disappear and reappear when the details appear?
-        titleLabel.text = "Tonight's Dinner?"
-        tapButton.isHidden = false
-    }
-    
-    // MARK:- Navigation
-    override func prepare(for segue: UIStoryboardSegue,
-                             sender: Any?) {
-        if segue.identifier == "ShowDetail" {
-        let detailViewController = segue.destination
-                                      as! DetailViewController
-        
-        let dinnerResult = dinner
-        detailViewController.dinnerResult = dinnerResult
-        }
+//        titleLabel.text = "Tonight's Dinner?"
+//        tapButton.isHidden = false
+//        dinnerDetails.isHidden = false
     }
     
 }
