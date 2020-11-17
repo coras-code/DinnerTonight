0d83ed962bd44903edf8a4110f8dac68b4372d18
Cora-Jane Meade <corajanemeade@gmail.com>
2020-11-17 14:17:34 +0000

    Initial Commit

---
+++ DinnerTonight/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  DinnerTonight
+//
+//  Created by My Apps on 17/11/2020.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var dinnerResult: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DinnerTonight/ViewController.swift
+++ DinnerTonight/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var dinner = ""
 
+//let ballArray = [#imageLiteral(resourceName: "Background22"),#imageLiteral(resourceName: "Background15"),#imageLiteral(resourceName: "Background20"),#imageLiteral(resourceName: "Background12"),#imageLiteral(resourceName: "Background18"),#imageLiteral(resourceName: "Background21"),#imageLiteral(resourceName: "Background15"),#imageLiteral(resourceName: "Background20"),]
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var dinnerDetails: UIButton!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        imageView.image = UIImage(named:"Background11")!
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        updateUI()
+        let dinners = ["Spag Bol", "Creamy Mushroom Pasta", "Stuffed Peppers", "Meatballs", "Portobello Mushroom Pesto Pasta", "Teriyaki Salmon", "Vegetable Tagine with Couscous", "Chilli Con Carne", "Chorizo Prawn Linguine"]
+        dinner = dinners[Int.random(in: 0...(dinners.count - 1))]
+        let pictureNumber = Int.random(in: 2...22)
+        imageView.image = UIImage(named:"Background\(pictureNumber)")!
+    
+        dinnerDetails.setTitle(dinner, for: .normal)
+        
+        //Disappear and reappear when the details appear?
+        //titleLabel.text = ""
+        //tapButton.isHidden = true
+        
+    }
+    
+    @IBAction func dinnerDetailsPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "ShowDetail", sender: sender)
+    }
+    
+    func updateUI() {
+        imageView.contentMode = .scaleAspectFill
+    
+        //Adjust postion
+        let positionArray = [7, 4, 8]
+        let randomPosition = positionArray[Int.random(in: 0...2)]
+        imageView.contentMode = UIView.ContentMode(rawValue: randomPosition)! //probelms with image 7, image 1
+        imageView.clipsToBounds = true
+                          
+        // imageView.contentMode = .left   //7
+        // imageView.contentMode = .center //4
+        // imageView.contentMode = .right //8
+        
+        //Disappear and reappear when the details appear?
+        titleLabel.text = "Tonight's Dinner?"
+        tapButton.isHidden = false
+    }
+    
+    // MARK:- Navigation
+    override func prepare(for segue: UIStoryboardSegue,
+                             sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+        let detailViewController = segue.destination
+                                      as! DetailViewController
+        
+        let dinnerResult = dinner
+        detailViewController.dinnerResult = dinnerResult
+        }
+    }
+    
 }
-
